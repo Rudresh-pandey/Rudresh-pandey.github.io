e00b608e85d21d121c216e9c4c7c2201eaf2c206
Rudresh Pandey <rudreshpandey2002@gmail.com>
2023-07-11 12:44:27 +0530

    check1

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+
 import "./App.css";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,5 @@
 import { Link } from "react-router-dom";
 import "./navbar.css";
-import { FiSun } from "react-icons/fi";
-import { BsMoon } from "react-icons/bs";
 
 type navProps = {
   active: string;
